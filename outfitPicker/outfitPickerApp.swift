11f5e97b88d103f184a8bffa3128fac64846bdc9
connorcampagna <connorcampagna36@outlook.com>
2026-01-06 16:44:00 +0000

    Initial Commit

---
+++ outfitPicker/outfitPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  outfitPickerApp.swift
+//  outfitPicker
+//
+//  Created by Connor Campagna on 06/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct outfitPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
